02d7a7b955f589ec3231a7e70b7194cde394657e
ThomasPlunkett <thomas.plunkett.music@gmail.com>
2021-05-26 10:44:49 -0400

    creating new repo

---
+++ build/precache-manifest.2268453c21c42dac7e1224dff8e8d7fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9169b71ba2d61b75d6cacceb81a7ae74",
+    "url": "/ThomasPlunkett/19-HW-User-Directory/index.html"
+  },
+  {
+    "revision": "c85635143026af4a75f7",
+    "url": "/ThomasPlunkett/19-HW-User-Directory/static/css/2.9233ae65.chunk.css"
+  },
+  {
+    "revision": "5bcc18fb2a5a0a9066b6",
+    "url": "/ThomasPlunkett/19-HW-User-Directory/static/css/main.3ae1f97e.chunk.css"
+  },
+  {
+    "revision": "c85635143026af4a75f7",
+    "url": "/ThomasPlunkett/19-HW-User-Directory/static/js/2.0a6ff45d.chunk.js"
+  },
+  {
+    "revision": "5bcc18fb2a5a0a9066b6",
+    "url": "/ThomasPlunkett/19-HW-User-Directory/static/js/main.e6baf34e.chunk.js"
+  },
+  {
+    "revision": "b9547d78b27401160411",
+    "url": "/ThomasPlunkett/19-HW-User-Directory/static/js/runtime~main.49fd79d8.js"
+  }
+]);
