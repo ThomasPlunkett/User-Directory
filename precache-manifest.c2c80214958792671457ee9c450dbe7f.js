b4317c3938f0a198ef3fc836259bad98a3751b81
ThomasPlunkett <thomas.plunkett.music@gmail.com>
2021-05-26 10:47:17 -0400

    Updates

---
+++ precache-manifest.c2c80214958792671457ee9c450dbe7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4af35e005ee5dbba7ead7f2dece9381a",
+    "url": "/ThomasPlunkett/User-Directory/index.html"
+  },
+  {
+    "revision": "c85635143026af4a75f7",
+    "url": "/ThomasPlunkett/User-Directory/static/css/2.9233ae65.chunk.css"
+  },
+  {
+    "revision": "121623db353bdd4471b7",
+    "url": "/ThomasPlunkett/User-Directory/static/css/main.3ae1f97e.chunk.css"
+  },
+  {
+    "revision": "c85635143026af4a75f7",
+    "url": "/ThomasPlunkett/User-Directory/static/js/2.0a6ff45d.chunk.js"
+  },
+  {
+    "revision": "121623db353bdd4471b7",
+    "url": "/ThomasPlunkett/User-Directory/static/js/main.cc908437.chunk.js"
+  },
+  {
+    "revision": "b265c40ee7e082ddfb5c",
+    "url": "/ThomasPlunkett/User-Directory/static/js/runtime~main.23e049b4.js"
+  }
+]);
